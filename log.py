f1b7b113932828b67c716314cb96921236cb2a05
Ítalo Domingos <43156003+italodomingos@users.noreply.github.com>
2022-07-27 17:40:38 -0300

    Ajustes

--- log.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-
-def write(text):
-    with open(r"C:\UAUAPIUPDATER\log.txt", "a") as log:
-        time_now = datetime.datetime.now().strftime('%d/%m/%Y %H:%M')
-        log.write(f'{time_now} {text}')
